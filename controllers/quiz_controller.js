b988ef0f78cb3c5005bf156979b1f39ffbe48e8b
Jorge Vayá Utiel <jorgevayautiel@gmail.com>
2015-07-15 08:34:58 +0200

    Despliegue BD Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,33 @@
+var models=require('../models/models.js');
+
+
 //GET /quizes/question
-exports.question = function(req,res) {
+exports.question=function(req,res) {
+	models.Quiz.findAll().success(function(quiz) {
+		res.render('quizes/question', {pregunta: quiz[0].pregunta})
+	})
+};
+
+//GET /quizes/answer
+
+exports.answer= function(req,res) {
+	models.Quiz.findAll().success(function(quiz) {
+		if (req.query.respuesta==quiz[0].respuesta) {
+			res.render('quizes/answer', { respuesta: 'Correcto'});
+		}
+		else
+		{
+			res.render('quizes/answer', { respuesta: 'Incorrecto'});
+		}		
+
+	})
+
+};
+
+
+
+
+/*exports.question = function(req,res) {
 	res.render('quizes/question', {pregunta: 'Capital de Italia'});
 };
 
@@ -13,4 +41,4 @@ exports.answer=function(req,res) {
 	}	
 	
 
-};
+};*/
