2e4dd1d10f2d38b0c25758335f1747293d84ca0e
Jorge Vayá Utiel <jorgevayautiel@gmail.com>
2015-07-16 08:45:07 +0200

    Busqueda

--- routes/index.js
+++ routes/index.js
@@ -3,17 +3,26 @@ var router = express.Router();
 
 var quizController=require('../controllers/quiz_controller');
 
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('home', { title: 'Quiz' });
 });
+
+router.get('/busca', function(req, res) {
+  res.render('find');
+});
+
+router.param('quizId', quizController.load);
 
 router.get('/author', function(req,res) {
    res.render('author');
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
 
+//Rutas quizes
+router.get('/quizes',	quizController.index);
+router.get('/quizes/:quizId(\\d+)',	quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',	quizController.answer);
 
 module.exports = router;
